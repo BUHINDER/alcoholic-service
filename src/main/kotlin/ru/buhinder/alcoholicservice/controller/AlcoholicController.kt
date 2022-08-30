1e126d477413eff0cb1a8af824035fde7280c1d0
Alexander Kunakov <kf@kode.ru>
2022-08-30 21:14:16 +0300

    [BUH-T-49] Add own info endpoint

--- src/main/kotlin/ru/buhinder/alcoholicservice/controller/AlcoholicController.kt
+++ src/main/kotlin/ru/buhinder/alcoholicservice/controller/AlcoholicController.kt
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import ru.buhinder.alcoholicservice.dto.response.AlcoholicResponse
 import ru.buhinder.alcoholicservice.service.AlcoholicService
+import java.security.Principal
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/alcoholic/alcoholic")
@@ -17,6 +19,11 @@ class AlcoholicController(
     @GetMapping("/{email}")
     fun get(@PathVariable email: String): Mono<AlcoholicResponse> {
         return alcoholicService.get(email)
+    }
+
+    @GetMapping("/own")
+    fun getOwnInfo(principal: Principal): Mono<AlcoholicResponse> {
+        return alcoholicService.get(UUID.fromString(principal.name))
     }
 
 }
